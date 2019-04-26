d7d1a2b175840039e7006f053079566b77053272
mithunpaul08 <mithunpaul08@gmail.com>
2019-04-26 15:36:39 -0700

    ii commiting from server. mostly will have to delete the echo lineswq

--- amalgram/pysupersensetagger-2.0/sst.sh
+++ amalgram/pysupersensetagger-2.0/sst.sh
@@ -7,8 +7,7 @@ set -eu
 set -o pipefail
 
 input=$1 # word and POS tag on each line (tab-separated)
-echo "value of input file is $input"
-./predict_sst.sh $input > ./outputs/$input.pred.tags
+./predict_sst.sh $input > $input.pred.tags
 
 src/tags2sst.py -l $input.pred.tags > $input.pred.sst
 

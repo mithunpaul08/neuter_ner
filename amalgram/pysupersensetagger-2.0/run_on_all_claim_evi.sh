c6d35813f8e530a3745071b732a1e9d40377bcf2
mithun <mithunpaul08@gmail.com>
2019-04-25 18:25:33 -0700

    changed the name of input file in shell script runallclaimevi.sh

--- amalgram/pysupersensetagger-2.0/run_on_all_claim_evi.sh
+++ amalgram/pysupersensetagger-2.0/run_on_all_claim_evi.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-for f in $(ls ./inputs/); do
-    $(./sst.sh ./inputs/$f)
+for f in $(ls ./input_to_sstagger_output_from_pos_tagger/); do
+    $(./sst.sh ./input_to_sstagger_output_from_pos_tagger/$f)
 done

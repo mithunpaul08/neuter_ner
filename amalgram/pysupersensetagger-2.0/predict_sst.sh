1520b4c230d91a5f938bfee6989e49a80f05e31e
mithunpaul08 <mithunpaul08@gmail.com>
2019-04-29 14:53:49 -0700

    code that runs fever train from input folder. produces 2 sets of output, in input folder and output folder

--- amalgram/pysupersensetagger-2.0/predict_sst.sh
+++ amalgram/pysupersensetagger-2.0/predict_sst.sh
@@ -4,4 +4,4 @@ IN=$1
 model=sst.model
 
 # prediction only
-	python2.7 src/main.py --cutoff 5 --YY tagsets/bio2gNV --defaultY O --predict $IN --debug --load $model --bio NO_SINGLETON_B --cluster-file mwelex/yelpac-c1000-m25.gz --clusters --lex mwelex/{semcor_mwes,wordnet_mwes,said,phrases_dot_net,wikimwe,enwikt}.json --input_folder /net/kate/storage/work/mithunpaul/neuter_ner_fever_training/outputs --output_folder outputs_sstagged
+	python2.7 src/main.py --cutoff 5 --YY tagsets/bio2gNV --defaultY O --predict $IN --debug --load $model --bio NO_SINGLETON_B --cluster-file mwelex/yelpac-c1000-m25.gz --clusters --lex mwelex/{semcor_mwes,wordnet_mwes,said,phrases_dot_net,wikimwe,enwikt}.json --input_folder /net/kate/storage/work/mithunpaul/neuter_ner_fever_training/outputs --output_folder outputs_sstagged --use_xargs 
--- amalgram/pysupersensetagger-2.0/src/discriminativeTagger.pyx
+++ amalgram/pysupersensetagger-2.0/src/discriminativeTagger.pyx
@@ -759,9 +759,9 @@ class DiscriminativeTagger(object):
             sent,derivation = decoder.send((sent,o0Feats))
             if print_predictions:
                 # print predictions
-                #print(sent)
-                #print()
-                f=open(output_file,"w")
+                print(sent)
+                print()
+                f=open(output_file,"w",0)
                 f.write(sent.__str__())
                 f.close()
         decoder.next()  # show summary statistics
@@ -874,6 +874,7 @@ def opts(actual_args=None):
     #for running over a directory of input files
     flag("input_folder", "Path to file with pos tags", default="input_to_sstagger_output_from_pos_tagger")
     flag("output_folder", "Path to folder where the tagged files will be kept", default="outputs_sstagged")
+    # boolflag means if the argument exists in command line, it means true, else don't even add it. its by default true
     boolflag("use_xargs", "For running code parallely. Either can be run from inside cython code or using xargs. Will feed one file each using shell script inside xargs", default=False)
 
     inflag("lex", "Lexicons to load for lookup features", nargs='*')
@@ -1044,7 +1045,7 @@ def main():
     files=os.listdir(args.input_folder)
     evalData = setup(args)
     if not (args.use_xargs):
-            for index,inputFile in enumerate(files):
+        for index,inputFile in enumerate(files):
                 fullpath=args.input_folder+"/"+inputFile
                 args.predict=fullpath
                 outputFileName=cwd+"/"+args.output_folder+"/"+inputFile+".pred.tags"
@@ -1053,10 +1054,10 @@ def main():
                     output=predict(args, _tagger_model,outputFileName,featurized_dataset=evalData)
     else:
         inputFile=args.predict
-        outputFileName=cwd+"/"+args.output_folder+"/"+inputFile+".pred.tags"
+        outputFileName=args.output_folder+"/"+inputFile+".pred.tags"
         # if the file already exists, leave it. It might have been written in a run before
-        if not (os.path.isfile(outputFileName)):
-                    output=predict(args, _tagger_model,outputFileName,featurized_dataset=evalData)
+        #if not (os.path.isfile(outputFileName)):
+        output=predict(args, _tagger_model,outputFileName,featurized_dataset=evalData)
 
 
 if __name__=='__main__':

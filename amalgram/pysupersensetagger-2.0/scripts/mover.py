1b1ca9deeedb0baca90cab44990c5b25645c881c
mithunpaul08 <mithunpaul08@gmail.com>
2019-05-07 22:06:37 -0700

    moved input files corresponding to already written tag files

--- amalgram/pysupersensetagger-2.0/scripts/mover.py
+++ amalgram/pysupersensetagger-2.0/scripts/mover.py
@@ -1,10 +1,19 @@
 #for all files which were already written, remove the corresponding input files. This way we can check if the code is even running at all or not
+#run this in folder where the sst.sh is
 import os,sys,subprocess
 from os import listdir
-inputFolder="/work/mithunpaul/neuter_ner_fever_training/amalgram/pysupersensetagger-2.0/input_to_sstagger_output_from_pos_tagger/"
-files=listdir(inputFolder)
+currentFolder=os.getcwd()
+folderWithPredTags=os.path.join(currentFolder,"outputs_sstagged/already_tagged")
+folderWithInputFilesToRemove=os.path.join(currentFolder,"input_to_sstagger_output_from_pos_tagger/")
+files=listdir(folderWithPredTags)
 for x in files:
     if x.endswith(".pred.tags"):
+        x_split=x.split(".pred.tags")
         print(x)
-        fullpath=os.path.join(inputFolder,x)
-        os.unlink(fullpath)
+        fileToRemoveFullpath=os.path.join(folderWithInputFilesToRemove,x_split[0])
+        print(fileToRemoveFullpath)
+        if os.path.isfile(fileToRemoveFullpath):
+            os.unlink(fileToRemoveFullpath)
+        else:
+            print("file not found")
+            print(fileToRemoveFullpath)

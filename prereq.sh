ac12cc1238fe7d3fe2e36731cccdf1b4a61fa957
mithun <mithunpaul08@gmail.com>
2019-10-27 00:24:29 -0700

    modified readme to look cleaner steps

--- prereq.sh
+++ prereq.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
 pip install tqdm
+pip install clean-text
 pip install git+https://github.com/myedibleenso/py-processors.git
-mkdir data
-wget -O data/file_to_delex.jsonl http://clulab.cs.arizona.edu/data/emnlp2019-delex/fever_dev_lex_3labels_26k_no_lists_evidence_not_sents.jsonl
-mkdir outputs
+mkdir -p outputs
+mkdir -p sstagged_files
+mkdir -p data
+
